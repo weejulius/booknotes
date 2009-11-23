2afb24025e423ba25812e2141097f0ba0d099dce
unknown <Julius.Yu@.cn.bleum.com>
2009-11-23 13:26:17 +0800

    user registry story:add new persistence

--- src/main/java/com/wee/netnotes/domain/user/DefaultUser.java
+++ src/main/java/com/wee/netnotes/domain/user/DefaultUser.java
@@ -1,5 +1,10 @@
 package com.wee.netnotes.domain.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by IntelliJ IDEA.
  * User: wee
@@ -7,9 +12,14 @@ package com.wee.netnotes.domain.user;
  * Time: 11:43:07 PM
  * To change this template use File | Settings | File Templates.
  */
+@Entity
 public class DefaultUser implements User {
-    private final String name;
-    private final String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  long id;   
+    private  String name;
+    private  String password;
+    
 
     public DefaultUser(final String name, final String password) {
         this.name = name;
--- src/test/java/com/wee/netnotes/infrastructure/TestUserRepository.java
+++ src/test/java/com/wee/netnotes/infrastructure/TestUserRepository.java
@@ -1,11 +1,49 @@
 package com.wee.netnotes.infrastructure;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import com.wee.netnotes.domain.user.User;
+import com.wee.netnotes.domain.user.UserRepositoryException;
+import com.wee.netnotes.domain.user.Users;
+import com.wee.netnotes.infrastructure.persistence.UserRepostiry;
+import com.wee.netnotes.infrastructure.persistence.hibernate.UserRepostiryHN;
+
 /**
- * Created by IntelliJ IDEA.
- * User: wee
- * Date: Nov 16, 2009
- * Time: 9:38:19 PM
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: wee Date: Nov 16, 2009 Time: 9:38:19 PM To
+ * change this template use File | Settings | File Templates.
  */
 public class TestUserRepository {
+    private UserRepostiry userRepository;
+
+    @Before
+    public void setup() {
+        userRepository = new UserRepostiryHN();
+    }
+
+    @Test
+    public void testStoreValidUsers() {
+        String[][] usersST = { { "weejud", "password" },
+                { "obamar", "iamobarmar" } };
+        testStoreUsersWithNameAndPassword(usersST);
+    }
+
+    private void testStoreUsersWithNameAndPassword(String[][] usersST) {
+        for (String[] userST : usersST) {
+            testStoreUserWithNameAndPassword(userST);
+        }
+    }
+
+    private void testStoreUserWithNameAndPassword(String[] userST) {
+        User user = Users.registry(userST[0], userST[1]);
+        userRepository.store(user);
+    }
+
+    @Test(expected = UserRepositoryException.class)
+    public void testStoreExistUser() {
+        String[] user = { "weejud", "password" };
+        String[][] users = { user, user };
+
+        testStoreUsersWithNameAndPassword(users);
+    }
 }
